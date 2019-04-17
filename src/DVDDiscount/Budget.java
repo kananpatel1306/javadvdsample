963c85d68d10a21bad5485316ce866d934a48464
Kanan <kananpatel1306@gmail.com>
2019-04-17 16:27:16 +1200

    Sample
    Structuring

--- src/DVDDiscount/Budget.java
+++ src/DVDDiscount/Budget.java
@@ -1,18 +1,19 @@
 package DVDDiscount;
 
 public class Budget extends DVD
-{
-    final double discount=0.5;
-public Budget()
-{
-    super();
-}
-public Budget(double ePrice)
-{
-    super(ePrice);
-}
-public double discount()
-{
-    return discount*price;
-}
+    {
+        final double discount=0.5;
+    public Budget()
+    {
+        super();
+    }
+
+    public Budget(double ePrice)
+    {
+        super(ePrice);
+    }
+    public double discount()
+    {
+        return discount*price;
+    }
 }
